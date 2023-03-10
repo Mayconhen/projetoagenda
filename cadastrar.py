bf1091eff2833de6256a7bf0ee0cdfd1d2974816
Maycon Henrique <mayconhenrique360@gmail.com>
2023-03-10 15:09:39 -0300

    atualizar

--- cadastrar.py
+++
@@ -1,8 +0,0 @@
-class cadastrar:
-
-    def __init__(self, nome, celular):
-        self._nome = nome
-        self._celular = celular
-
-
-
--- main.py
+++ main.py
@@ -1,33 +1,28 @@
-from cadastrar import cadastrar
-
-def retornapessoa(pessoanome, pessoacelular):
-    print("Nome da pessoa:{}\nCelular da pessoa:{}".format(pessoanome, pessoacelular))
-
 def cadastrarpessoa():
-    pessoanome = input("Digite o nome do contato: ")
-    pessoacelular = input("Digite o telefone da pessoa: ")
-    print("Pessoa cadastrada!\n")
-    return pessoanome, pessoacelular
-
+    adclista = []
+    nome = input("Digite o nome: ")
+    celular = input("Digite o celular: ")
+    adclista.append(nome)
+    adclista.append(celular)
+    listas.append(adclista)
+    print("Usuário criado!")
+    
+def retornarpessoas(lista):
+    for pessoas in lista:
+        print(pessoas)
+            
 if __name__ == "__main__":
+    listas = [[]]
 
     while True:
         menu = input("1.Entrar no menu\n2.Sair do programa\n")
 
         if menu != '2':
+            opcao = input("Escolha uma das opcoes:\n1.Cadastrar Pessoa na Agenda\n2.Listar pessoas\n")
 
-            opcao = input("Escolha uma das opcoes:\n1.Cadastrar Pessoa na Agenda\n2.Alterar dados da Pessoa\n3.Listar pessoas\n")
             if opcao == '1':
                 cadastrarpessoa()
-
             if opcao == '2':
-                novonome = input("Digite o novo nome do contato: ")
-                novocelular = input("Digite o novo telefone da pessoa: ")
-                pessoanome = novonome
-                pessoacelular = novocelular
-            
-            if opcao == '3':
-                retornapessoa(pessoanome, pessoacelular)
-
+                retornarpessoas(listas[2])
         else:
             exit()
